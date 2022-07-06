33961c4037768aec3c59db652f4358707e4adae6
Yisell Sarasua <yisell.sarasua@gmail.com>
2022-07-06 13:41:44 -0300

    evaluando diferentes maneras de procesar los datos recopilados

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,73 @@
 // 1. Identificar el valor que esto clickeando
 
-const valorBoton = document.querySelectorAll ('.numero');
-const operacionSeleccionada = document.querySelectorAll ('.operacion');
+// Creo variables donde guardo los datos obtenidos desde el html de los botones
+const botonNumero = document.querySelectorAll ('.numero');
+const botonOperacion = document.querySelectorAll ('.operacion');
+var botonResultado = document.getElementById('resultado');
 
-/*valorBoton.addEventListener('click', () => {
-    console.log (valorBoton);
+const botonBorrar = document.getElementById('borrar');
+const botonBorrarUno= document.querySelectorAll('borrarUno');
+//Creo valiables donde guardo los datos obtenidos desde el html del display
+var resultadoAnterior=document.getElementById("resultadoAnterior"); 
+var resultadoActual=document.getElementById("resultadoActual");
 
-})
-*/
+var operacionActual = "";
+var operacionAnterior = "";
+var operacion = undefined;
 
-valorBoton.forEach(boton => {
 
-   // console.log(valorBoton); .forEach es una como un for, que recorre ValorBotomo como si se tratara de un array.
 
-    boton.addEventListener('click', () =>{
+
+botonNumero.forEach(function (boton) {
+
+   // console.log(valorBoton); .forEach es una como un for, que recorre botonNumero como si se tratara de un array.
+
+    boton.addEventListener('click', function () {
         console.log(boton.innerHTML);
-
+        //agregarNumero(boton.innerHTML); // Cada vez que hago click llamo a una funcion.
     })
 
     // .addEventListener espera la lectura de un evento, en este caso un click y cuando el evento se genera, muesta el valor HTML del mismo con .innerHTML
     
 });
 
-operacionSeleccionada.forEach(operacion => {
 
-    operacion.addEventListener('click', () => {
-        console.log(operacion.innerHTML);
-    });
-});
+botonOperacion.forEach(function (boton) {
+
+     boton.addEventListener('click', function () {
+         console.log(boton.innerHTML);
+
+         if (boton.innerHTML == "=" ){
+            console.log("finalizar calculo");
+
+         } else if (boton.innerHTML == "+" ){
+            console.log("Sumar");
+
+         } else  if (boton.innerHTML == "-" ){
+            console.log("restar");
+
+         } else  if (boton.innerHTML == "%" ){
+            console.log("dividir");
+
+         }
+         //agregarOperacion(boton.innerHTML); // Cada vez que hago click llamo a una funcion.
+     })
+ 
+ });
+
+
+ botonResultado.onclick = function() {
+    console.log("mostrar resultado");
+};
+ 
+
+ 
+
+
+
+
+
+
+
+
 
