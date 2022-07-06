8cdb85804255eab28aebd4fa906c632bdb387c42
Yisell Sarasua <yisell.sarasua@gmail.com>
2022-07-06 02:16:31 -0300

    agregue funion para leer los botones de las operaciones

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
 // 1. Identificar el valor que esto clickeando
 
 const valorBoton = document.querySelectorAll ('.numero');
+const operacionSeleccionada = document.querySelectorAll ('.operacion');
 
 /*valorBoton.addEventListener('click', () => {
     console.log (valorBoton);
@@ -9,12 +10,22 @@ const valorBoton = document.querySelectorAll ('.numero');
 */
 
 valorBoton.forEach(boton => {
-    console.log(valorBoton);
+
+   // console.log(valorBoton); .forEach es una como un for, que recorre ValorBotomo como si se tratara de un array.
 
     boton.addEventListener('click', () =>{
         console.log(boton.innerHTML);
 
     })
+
+    // .addEventListener espera la lectura de un evento, en este caso un click y cuando el evento se genera, muesta el valor HTML del mismo con .innerHTML
     
+});
+
+operacionSeleccionada.forEach(operacion => {
+
+    operacion.addEventListener('click', () => {
+        console.log(operacion.innerHTML);
+    });
 });
 
